7a73e8bf9f2cf5829a1637549d88f7e11b1da128
Squirtle0204 <squirtle7865@gmail.com>
2025-08-28 00:39:34 +0530

    Time: 15 ms (56.68%) | Memory: 16.7 MB (34.23%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<int>upper(26,0),lower(26,0);
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
-                lower[s[i]-'a']++;
-                s[i]='#';
+        vector<int> upper(26, 0), lower(26, 0);
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u') {
+                lower[s[i] - 'a']++;
+                s[i] = '#';
             }
-             if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
-                upper[s[i]-'A']++;
-                s[i]='#';
+            if (s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U') {
+                upper[s[i] - 'A']++;
+                s[i] = '#';
             }
-
         }
         string ans;
-        for(int i=0;i<26;i++){
-            char c='A'+i;//upper
-            while(upper[i]){
-                ans+=c;
-                upper[i]--;
-            }
+        for (int i = 0; i < 26; i++) {
+            char c = 'A' + i;
+            while (upper[i]--) ans += c;
         }
-          for(int i=0;i<26;i++){
-            char c='a'+i;//lower
-            while(lower[i]){
-                ans+=c;
-                lower[i]--;
+        for (int i = 0; i < 26; i++) {
+            char c = 'a' + i;
+            while (lower[i]--) ans += c;
+        }
+        // Reinserting vowels using first and second pointers
+        int first = 0, second = 0;
+        while (second < ans.size()) {
+            if (s[first] == '#') {
+                s[first] = ans[second];
+                second++;
             }
-          }
-            int first=0,second=0;
-            while(second<ans.size()){
-                if(s[first]=='#'){
-                    s[first]=ans[second];
-                    second++;
-                }
-                first++;
-            }
-    
-          
-            return s;
+            first++;
+        }
+        return s;
     }
 };
