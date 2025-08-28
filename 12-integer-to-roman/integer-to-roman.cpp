52664348d9f2b1ef705984f4050d1302877308b9
Squirtle0204 <squirtle7865@gmail.com>
2025-08-28 23:42:17 +0530

    Time: 3 ms (63.34%) | Memory: 12 MB (60.69%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -5,7 +5,7 @@ public:
         vector<string>romanS={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         vector<int>values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
     
-        for(int i=0;i<romanS.size();i++){
+        for(int i=0;i<romanS.size();++i){
             while(num>=values[i]){
                 num-=values[i];
                 result+=romanS[i];
