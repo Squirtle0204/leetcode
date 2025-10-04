6cdabfb716211b34fbe84d85a3264280dd071a67
Hardik Pandey <squirtle7865@gmail.com>
2025-10-04 15:02:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.44%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>count;
+        for(char c:s){
+            count[c]++;
+        }
+        for(char c:t){
+            if(count.find(c)==count.end()||count[c]==0){
+                return false;
+            }
+            count[c]--;
+        }
+        for(auto &entry:count){
+            if(entry.second!=0){
+                return false;
+            }
+           
+        }
+         return true;
+    }
+};
