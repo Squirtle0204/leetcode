27a0e33fa439194126f0c7be5ec9bf0c8077ad1f
Hardik Pandey <squirtle7865@gmail.com>
2025-10-08 04:19:47 +0530

    Time: 1045 ms (39.61%) | Memory: 7.6 MB (99.61%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool checkPerfectNumber(int num) {
          if (num <= 0) return false; 
-
-    int sum = 0;
+        int sum = 0;
     for (int i = 1; i <= num / 2; i++) {
         if (num % i == 0) { 
             sum += i;
         }
     }
     return sum == num;
-}
-    
+}   
 };
