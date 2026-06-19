4f3034b69c9234053bec962aee903c14f2c91a01
Hardik Pandey <squirtle7865@gmail.com>
2026-06-20 02:39:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (44.61%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int curr=0;
+        int maxi=0;
+
+        for(int it: gain){
+            curr+=it;
+            maxi=max(maxi,curr);
+        }
+        return maxi;
+        
+    }
+};
