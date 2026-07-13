e39e098fd96f4ca19b08e3760aafa0e471412241
Hardik Pandey <squirtle7865@gmail.com>
2026-07-13 06:09:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (60.05%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -4,12 +4,16 @@ public:
         int maxp=nums[0],minp=nums[0],result=nums[0];
 
         for(int i=1;i<nums.size();i++){
-           if (nums[i]<0){
-            swap(maxp,minp);
-           }
-           maxp=max(nums[i],maxp*nums[i]);
-           minp=min(nums[i],minp*nums[i]);
-           result=max(result,maxp);
+            if(nums[i]<0){
+                swap(maxp,minp);
+                
+            }
+            
+            maxp=max(nums[i],maxp * nums[i]);
+            minp=min(nums[i],minp * nums[i]);
+            result=max(result,maxp); 
+            
+            
         }
         return result;
         
