dffd4540a83f0fb7510542aad74cf41fc1a76c5d
Hardik Pandey <squirtle7865@gmail.com>
2026-07-14 08:26:25 +0530

    Time: 2 ms (54.83%) | Memory: 30.2 MB (87.63%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,66 +1,20 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int>pos;
-        vector<int>neg;
-        vector<int>ans;
-
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<0){
-                neg.push_back(nums[i]);
-            }
-            else{
-                pos.push_back(nums[i]);
-            }
+        vector<int>ans(nums.size());
+       int l=0,r=nums.size()-1;
+        for(int k = nums.size()-1;k>=0;k--){
+        if(abs(nums[l])> abs(nums[r])){
+            ans[k]= nums[l]* nums[l];
+            l++;
         }
-
-        if(neg.size()==0){
-            for(int i=0;i<pos.size();i++){
-            pos[i]=pos[i]*pos[i];
-            }
-            return pos;
-        
+        else{
+            ans[k]= nums[r]* nums[r];
+            r--;
         }
-
-        if(pos.size()==0){
-            for(int i=0;i<neg.size();i++){
-            neg[i]=neg[i]*neg[i];
-            }
-            reverse(neg.begin(),neg.end());
-            return neg;
-            
-        }
-        for(int i= 0;i<pos.size();i++){
-            pos[i]=pos[i]*pos[i];
-        }
-
-        for(int i=0;i<neg.size();i++){
-            neg[i]=neg[i]*neg[i];
-        }
-        reverse(neg.begin(),neg.end());
-
-        int i=0,j=0;
-
-        while(i<pos.size() && j<neg.size()){
-             if(pos[i]<neg[j]){
-               ans.push_back(pos[i]);
-               i++;
-             }
-             else {
-                ans.push_back(neg[j]);
-                j++;
-             }
-        }
-
-        while(i<pos.size()){
-            ans.push_back(pos[i]);
-            i++;
-        }
-        while(j<neg.size()){
-            ans.push_back(neg[j]);
-            j++;;
-        }
-        return ans;
-        
+       
+       }
+       
+       return ans;
     }
 };
