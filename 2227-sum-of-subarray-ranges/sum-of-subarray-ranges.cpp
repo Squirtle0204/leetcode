887093a3be140bc614de1b884adae749dfe67883
Hardik Pandey <squirtle7865@gmail.com>
2026-07-14 07:50:13 +0530

    Time: 2 ms (78.81%) | Memory: 16 MB (29.42%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,17 +1,92 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        long long sum=0;
+   vector<int>nse(vector<int> & nums){
+    int n= nums.size();
+    vector<int>nse(n);
+    stack<int> st;
 
-        for(int i=0;i<nums.size();i++){
-            int smallest=nums[i],largest=nums[i];
-            for(int j=i+1;j<nums.size();j++){
-            
-                largest= max(largest,nums[j]);
-                smallest = min(smallest,nums[j]);
-                sum += (largest - smallest);
-            }
+    for(int i =n-1;i>=0;i--){
+        while(!st.empty() && nums[st.top()]>= nums[i]){
+            st.pop();
         }
-        return sum;
+        nse[i]= st.empty()? n: st.top();
+        st.push(i);
+    }
+    return nse;
+   }
+
+   vector<int>psee(vector<int> & nums){
+    int n = nums.size();
+    vector<int>psee(n);
+    stack<int>st;
+
+    for(int i=0;i<n;i++){
+        while(!st.empty() && nums[st.top()]>nums[i]){
+            st.pop();
+        }
+        psee[i]= st.empty()?-1:st.top();
+        st.push(i);
+    }
+    return psee;
+   }
+
+   vector<int>nge(vector<int> & nums){
+    int n= nums.size();
+    vector<int>nge(n);
+    stack<int>st;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && nums[st.top()]<=nums[i]){
+            st.pop();
+
+        }
+        nge[i]=st.empty()? n:st.top();
+        st.push(i);
+    }
+    return nge;
+   }
+
+   vector<int>pgee(vector<int> & nums){
+    int n= nums.size();
+    vector<int>pgee(n);
+    stack<int>st;
+    for(int i =0;i<n;i++){
+        while(!st.empty()&& nums[st.top()]<nums[i]){
+           
+            st.pop();
+        }
+        pgee[i]=st.empty()?-1:st.top();
+        st.push(i);
+    }
+    return pgee;
+   }
+
+   long long subarraymin(vector<int> & nums){
+    vector<int>nsee=nse(nums);
+    vector<int>pseee=psee(nums);
+    long long smallest=0;
+    for(int i=0;i<nums.size();i++){
+        int left= i-pseee[i];
+        int right= nsee[i]-i;
+        smallest= (smallest +(left * right * 1LL * nums[i]));
+   }
+   return smallest;
+   }
+   
+
+   long long subarraymax(vector<int> & nums){
+    vector<int> pgeee= pgee(nums);
+    vector<int>ngee=nge(nums);
+    long long largest=0;
+    for(int i=0;i<nums.size();i++){
+        int left= i-pgeee[i];
+        int right= ngee[i]-i;
+        largest= (largest +( left * right * 1LL * nums[i]));
+    }
+    return largest;
+   }
+   
+    long long subArrayRanges(vector<int>& nums) {
+        return ( subarraymax(nums)-subarraymin(nums));
+        
     }
 };
