562d3418cda16b01cabaade88d7fc52f2f799470
Hardik Pandey <squirtle7865@gmail.com>
2026-07-20 16:05:13 +0530

    Time: 86 ms (18.22%) | Memory: 193.4 MB (55.85%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,47 +1,45 @@
 class Solution {
 public:
-    
-    vector<int>nse(vector<int> & arr){
-        int n= arr.size();
-        vector<int>nse(n);
-        stack<int>st;
 
-        for(int i = n-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()]>= arr[i]){
-                st.pop();
-            }
-            nse[i]= st.empty()? n: st.top();
-            st.push(i);
+  vector<int> nse(vector<int> & arr){
+    stack<int>st;
+    vector<int>nse(arr.size());
+     int n= arr.size();
+     for(int i=n-1;i>=0;i--){
+        while(!st.empty() && arr[st.top()]>=arr[i]){
+            st.pop();
         }
-        return nse;
-    }
+        nse[i] = st.empty()? n:st.top();
+        st.push(i);
+     }
+  return nse;
+  }
 
-    vector<int>psee(vector<int> & arr){
-        int n= arr.size();
-        vector<int>psee(n);
-        stack<int>st;
-        for(int i=0;i<n;i++){
-            while(!st.empty()&& arr[st.top()]> arr[i]){
-                st.pop();
-            }
-            psee[i]=st.empty()? -1: st.top();
-            st.push(i);
+  vector<int>psee(vector<int> & arr){
+    stack<int>st;
+    int n=arr.size();
+    vector<int>psee(n);
+    for(int i=0;i<n;i++){
+        while(!st.empty() && arr[st.top()]>arr[i]){
+            st.pop();
         }
-        return psee;
-    }
+        psee[i]=st.empty()?-1:st.top();
+        st.push(i);
 
-    
+    }
+    return psee;
+  }
     int sumSubarrayMins(vector<int>& arr) {
-         
-        vector<int> ns= nse(arr);
-        vector<int> ps=psee(arr);
-       int total = 0;
-        int modu=(int)(1e9 + 7);
-        for(int i=0;i< arr.size();i++){
-            int left= i-ps[i];
-            int right = ns[i]-i;
-            total = (total + (right * left*1LL * arr[i])%modu)%modu;
+        vector<int>nsee=nse(arr);
+        vector<int>pseee=psee(arr);
+        int total=0;
+        int modu=(int)(1e9 +7);
+        for(int i=0;i<arr.size();i++){
+            int left= i-pseee[i];
+            int right=nsee[i]-i;
+            total =(total+(left * right*1LL *arr[i])%modu)%modu;
         }
+        
         return total;
     }
 };
