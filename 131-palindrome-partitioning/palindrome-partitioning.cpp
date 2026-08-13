58485229d0062ae54f7a2e8bd686014cc82e8a7e
Hardik Pandey <squirtle7865@gmail.com>
2026-08-13 23:46:49 +0530

    Time: 64 ms (22.72%) | Memory: 61.7 MB (47.27%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
- void partition(int index,string s,vector<vector<string>>&result,vector<string>&path){
-    if(index==s.size()){
-        result.push_back(path);
+
+   bool ispalindrome(string s, int start, int end){
+    while(start<=end){
+        if(s[start]!=s[end])return false;
+        start++,end--;
+    }
+    return true;
+   }
+
+   void partitioning(int index, string s, vector<string>&ds, vector<vector<string>>&ans){
+    if(index == s.size()){
+        ans.push_back(ds);
         return;
     }
-    for(int i=index;i<s.size();i++){
-        if(ispalindrome(s,index,i)){
-            path.push_back(s.substr(index,i-index+1));
-            partition(i+1,s,result,path);
-            path.pop_back();
-        }
-    }
- }
-    bool ispalindrome(string s,int start,int end){
-        while(start<=end){
-            if(s[start++]!=s[end--]){
-                return false;
-            }
 
+    for(int i=index; i<s.size();i++){
+        if(ispalindrome(s,index,i)){
+            ds.push_back(s.substr(index,i-index+1));
+            partitioning(i+1, s,ds,ans);
+            ds.pop_back();
         }
-        return true;
     }
+   }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>result;
-        vector<string>path;
-        partition(0,s,result,path);
-        return result;
-        
+        vector<string>ds;
+        vector<vector<string>>ans;
+        partitioning(0,s,ds,ans);
+        return ans;
     }
 };
