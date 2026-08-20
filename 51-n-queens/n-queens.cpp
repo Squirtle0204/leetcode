a9cdb5fefe507c4179dba2e93480349daac8f156
Hardik Pandey <squirtle7865@gmail.com>
2026-08-21 01:12:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (94.13%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-void solve(vector<vector<string>>&ans,vector<int>&leftrow,vector<int>&upperdiagonal,vector<int>&lowerdiagonal,int n,int col,vector<string>&board){
-    if(col==n){
-        ans.push_back(board);
-        return;
-    }
 
-    for(int row=0;row<n;row++){
-        if(leftrow[row]==0 && upperdiagonal[(n-1)+(col-row)]==0 && lowerdiagonal[row+col]==0){
-            board[row][col]='Q';
-            leftrow[row]=1;
-            upperdiagonal[(n-1)+(col-row)]=1;
-            lowerdiagonal[col+row]=1;
-            solve(ans,leftrow,upperdiagonal,lowerdiagonal,n,col+1,board);
-            board[row][col]='.';
-             leftrow[row]=0;
-            upperdiagonal[(n-1)+(col-row)]=0;
-            lowerdiagonal[col+row]=0;
+    void solve(int col, vector<string>&board,vector<int>&leftrow,vector<int>&upperdiagonal,vector<int>&lowerdiagonal,int n,vector<vector<string>>&ans){
+
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+
+        for(int row=0;row<n;row++){
+            if(leftrow[row]==0 && upperdiagonal[n-1+col-row]==0 && lowerdiagonal[row+col]==0){
+                board[row][col]='Q';
+                leftrow[row]=1;
+                upperdiagonal[n-1+col-row]=1;
+                lowerdiagonal[row+col]=1;
+                solve(col+1,board,leftrow,upperdiagonal,lowerdiagonal,n,ans);
+                board[row][col]='.';
+                leftrow[row]=0;
+                upperdiagonal[n-1+col-row]=0;
+                lowerdiagonal[row+col]=0;
+            }
 
         }
     }
-    
-}
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
+        vector<int>leftrow(n,0);
+        vector<int>upperdiagonal(2*n-1,0);
+        vector<int>lowerdiagonal(2*n-1,0);
         vector<string>board(n);
         string s(n,'.');
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        vector<int>leftrow(n,0);
-        vector<int>upperdiagonal(2*n-1,0);
-        vector<int>lowerdiagonal(2*n-1,0);
-        solve(ans,leftrow,upperdiagonal,lowerdiagonal,n,0,board);
+        solve(0,board,leftrow,upperdiagonal,lowerdiagonal,n,ans);
         return ans;
         
     }
