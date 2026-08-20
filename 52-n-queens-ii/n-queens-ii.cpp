255f431ced176d8a492d536a0c94676852276a35
Hardik Pandey <squirtle7865@gmail.com>
2026-08-21 01:17:31 +0530

    Time: 2 ms (55.27%) | Memory: 9.1 MB (20.04%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+
+    void solve(int col, vector<string>&board,vector<int>&leftrow,vector<int>&upperdiagonal,vector<int>&lowerdiagonal,int n,vector<vector<string>>&ans,int &anss){
+
+        if(col == n){
+            ans.push_back(board);
+            anss++;
+            return;
+        }
+
+        for(int row=0;row<n;row++){
+            if(leftrow[row]==0 && upperdiagonal[n-1+col-row]==0 && lowerdiagonal[row+col]==0){
+                board[row][col]='Q';
+                leftrow[row]=1;
+                upperdiagonal[n-1+col-row]=1;
+                lowerdiagonal[row+col]=1;
+                solve(col+1,board,leftrow,upperdiagonal,lowerdiagonal,n,ans,anss);
+                board[row][col]='.';
+                leftrow[row]=0;
+                upperdiagonal[n-1+col-row]=0;
+                lowerdiagonal[row+col]=0;
+            }
+
+        }
+    }
+    
+    
+    int totalNQueens(int n) {
+        int anss=0;
+         vector<vector<string>>ans;
+        vector<int>leftrow(n,0);
+        vector<int>upperdiagonal(2*n-1,0);
+        vector<int>lowerdiagonal(2*n-1,0);
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        solve(0,board,leftrow,upperdiagonal,lowerdiagonal,n,ans,anss);
+        return anss;
+        
+        
+        
+    }
+};
