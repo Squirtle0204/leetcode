45a8a2cac03dedea628f88307eeb003d6fabbcc9
Hardik Pandey <squirtle7865@gmail.com>
2026-08-24 03:46:11 +0530

    Time: 11 ms (6.18%) | Memory: 14 MB (33.02%) - LeetSync

--- 2039-sum-game/sum-game.cpp
+++ 2039-sum-game/sum-game.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     bool sumGame(string num) {
-        int ql=0,qr=0;
-        int sl=0,sr=0;
+        int n = num.length();
+        int sumL = 0, sumR = 0;
+        int qL = 0, qR = 0;
 
-        for(int i=0;i<num.size();i++){
-            if(i<num.size()/2){
-                if(num[i]=='?'){
-                    ql++;
-                }
-                else{
-                    sl+=num[i]-'0';
-                }
-            }
-            else{
-                if(num[i]=='?'){
-                    qr++;
-
-                }
-                else{
-                    sr+=num[i]-'0';
-                }
+        for (int i = 0; i < n; i++) {
+            if (i < n / 2) {
+                if (num[i] == '?') qL++;
+                else sumL += num[i] - '0';
+            } else {
+                if (num[i] == '?') qR++;
+                else sumR += num[i] - '0';
             }
         }
-        if((ql+qr) % 2 !=0)return true;
 
+        // If total '?' is odd, Alice always wins
+        if ((qL + qR) % 2 != 0) return true;
 
-        return 2*(sl-sr)!= 9*(qr-ql);
-        
+        // Bob wins if the remaining unpaired '?' can exactly balance the sum difference
+        return 2 * (sumL - sumR) != 9 * (qR - qL);
     }
 };
