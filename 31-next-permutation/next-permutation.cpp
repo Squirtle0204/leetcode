1d27e77a465747c94516e03bbc23b7008c6f5e32
Hardik Pandey <squirtle7865@gmail.com>
2026-09-05 03:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (54.37%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int index=-1;
-        int n=nums.size();
 
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){//break point
-                index=i;
+        for(int i = nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index = i;
                 break;
             }
-
         }
+
         if(index==-1){
-            reverse(nums.begin(),nums.end());
-            return;
+           return reverse(nums.begin(),nums.end());
         }
 
-        for(int i=n-1;i>=index;i--){
+        for(int i = nums.size()-1;i>index;i--){
             if(nums[i]>nums[index]){
-                swap(nums[i],nums[index]); 
-                break; //find bigger but closeset
+                swap(nums[i],nums[index]);
+                break;
             }
         }
         reverse(nums.begin()+index+1,nums.end());
-        
     }
 };
