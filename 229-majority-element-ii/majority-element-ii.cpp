563d99050630eea3ec9b21d56a3e3dfb0d230421
Hardik Pandey <squirtle7865@gmail.com>
2026-09-05 02:07:04 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (50.05%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,57 +2,53 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int el1=0;
-        int cnt1=0;
         int el2=1;
+        int cnt1=0;
         int cnt2=0;
-        int n=nums.size();
+        int n = nums.size();
 
-        for(auto it: nums){
-            if( it == el1){
+        for(int it : nums){
+            if(it == el1){
                 cnt1++;
             }
-            else if( it==el2){
-                cnt2++;
-            }
-            else if( cnt1==0){
+            else if(it==el2)cnt2++;
+
+            else if(cnt1==0){
                 el1=it;
                 cnt1=1;
             }
-            else if( cnt2 ==0){
+            else if(cnt2==0){
                 el2=it;
                 cnt2=1;
             }
             else{
-            cnt1--;
-            cnt2--;
+                cnt1--;
+                cnt2--;
             }
         }
-
         //verify
-        int cnnt1=0,cnnt2=0;
 
-        for(auto it : nums){
-            if(it==el1){
-                cnnt1++;
+        int cntt1=0;
+        int cntt2=0;
+
+        for(int it : nums){
+            if(it == el1){
+                cntt1++;
             }
-            else if( it==el2){
-                cnnt2++;
+
+            if(it==el2){
+                cntt2++;
             }
         }
 
-        if(cnnt1>n/3 && cnnt2>n/3){
-            return{ el1,el2};
+        if(cntt1 > n/3  && cntt2> n/3){
+            return {el1,el2};
         }
-        else if(cnnt1>n/3){
-            return { el1};
-        }
-        else if(cnnt2>n/3){
-            return{el2};
-        }
-        else{
-            return {};
-        }
+        if (cntt1>n/3) return {el1};
+        if(cntt2>n/3)return {el2};
+
+
+         return {};
         
-       return {}; 
     }
 };
