c22c96e6a72a003301ff8c094bbefa4237182d76
Hardik Pandey <squirtle7865@gmail.com>
2026-09-08 23:23:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.51%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int ans =0;
         if(n<1000)return 0;
-
-        for(int i = 1;i<=n;i++){
-            ans = n-1000+1;
-
-        }
-        return ans;
+        else return n-999;
+        
     }
 };
