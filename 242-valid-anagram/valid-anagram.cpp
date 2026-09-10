756aba2fc5f7fbec78cf25ad64dbf97d7d335aac
Hardik Pandey <squirtle7865@gmail.com>
2026-09-11 04:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,10 +2,19 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())return false;
-         sort(s.begin(),s.end());
-         sort(t.begin(),t.end());
-        if(s==t)return true;
-        else return false;
-        
+        vector<int>freq(26,0);
+
+        for(auto i : s){
+             freq[i-'a']++;
+        }
+
+        for(auto i : t){
+            freq[i-'a']--;
+        }
+
+        for(int i : freq){
+            if(i!=0)return false;
+        }
+        return true;
     }
 };
