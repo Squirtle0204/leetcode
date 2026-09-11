4fa3eaf25594632d3949853cf791d0e98a6a558d
Hardik Pandey <squirtle7865@gmail.com>
2026-09-11 07:18:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (80.72%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string prefix=strs[0];
+        string prefix = strs[0];
 
-        for(int i=1;i<strs.size();i++){
-            while(strs[i].find(prefix)!=0){
-                prefix.pop_back();
-            }
-            if(prefix.empty()){
-                return "";
-            }
+        for(int i =1;i<strs.size();i++){
+           while( strs[i].find(prefix)!=0){
+            prefix.pop_back();
+           }
+           if(prefix.empty())return "";
         }
         return prefix;
-        
     }
 };
