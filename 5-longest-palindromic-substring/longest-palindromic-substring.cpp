8e79962fd4f1c68538fbc09349af1ff200ecabab
Hardik Pandey <squirtle7865@gmail.com>
2026-09-22 04:52:14 +0530

    Time: 6 ms (88.24%) | Memory: 9.3 MB (77.52%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-
-    bool ispalindrom(string & s ,int i,int j ){
-        int st = i,end=j;
-
-        while(st<=end){
-            if(s[st]!=s[end])return false;
-            else st++,end--;
-
-        }
-        return true;
-    }
     string longestPalindrome(string s) {
-      string ans="";
-
+        int maxlen=1;
+        int st=0;
       for(int i=0;i<s.size();i++){
-        for(int j=i;j<s.size();j++){
-            if(ispalindrom(s,i,j))
-            if(j-i+1>ans.size()){
-                ans = s.substr(i,j-i+1);
+        int l=i,r=i;//odd len
+        while(l>=0 && r<s.size() && s[l]==s[r]){
+             if(r-l+1>maxlen){
+                st=l;
+                maxlen= r-l+1;
+
+             }
+             l--;
+             r++;
+        }
+
+        //even
+        int k=i,j=i+1;
+        while(k>=0 && j<s.size()&& s[k]==s[j]){
+            if(j-k+1>maxlen){
+                st=k;
+                maxlen=j-k+1;
             }
+            k--;
+            j++;
         }
       }
-        
-        return ans;
+        return s.substr(st,maxlen);
     }
 };
