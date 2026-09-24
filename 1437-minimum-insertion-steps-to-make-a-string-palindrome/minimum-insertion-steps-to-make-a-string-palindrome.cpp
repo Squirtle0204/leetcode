967c78c2707718467dd9381592c7aeedb0c71e27
Hardik Pandey <squirtle7865@gmail.com>
2026-09-24 12:30:38 +0530

    Time: 35 ms (47.05%) | Memory: 31.6 MB (38.57%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
 
-    int lcs(string text1,string text2){
+    
+    int lps(string text1,string text2){
         int n = text1.size();
         int m=text2.size();
-
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        string tt= text2;
+        reverse(tt.begin(),tt.end());
+         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(text1[i-1]==text2[j-1]){
+                if(text1[i-1]==tt[j-1]){
                     dp[i][j]= 1+dp[i-1][j-1];
                 }
                 else{
@@ -18,11 +20,6 @@ public:
             }
         }
         return dp[n][m];
-    }
-    int lps(string text1,string text2){
-        string tt= text2;
-        reverse(tt.begin(),tt.end());
-        return lcs(text1,tt);
     }
      
     int minInsertions(string s) {
